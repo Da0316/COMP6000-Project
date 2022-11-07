3201da5e27588ebd08ee1ea291fa1dd710c2e687
Lkaasan <49850026+Lkaasan@users.noreply.github.com>
2022-11-07 15:47:33 +0000

    Added firebase connection

--- App.js
+++ App.js
@@ -6,6 +6,25 @@ import HomeScreen from './screens/homeScreen';
 import Login from './screens/login';
 
 //will need to import any new screens 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZonqQoeCQB8l_Cze3DRmCoZgeKMNvl10",
+  authDomain: "chat-screen-aca22.firebaseapp.com",
+  databaseURL: "https://chat-screen-aca22-default-rtdb.firebaseio.com",
+  projectId: "chat-screen-aca22",
+  storageBucket: "chat-screen-aca22.appspot.com",
+  messagingSenderId: "921334264296",
+  appId: "1:921334264296:web:523920ad64647e74b5f45d",
+  measurementId: "G-NDMVXR7NKP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 const Stack = createStackNavigator();
 
@@ -19,7 +38,6 @@ export default function App() {
         <Stack.Screen name="HomeScreen" component={HomeScreen}/>
       </Stack.Navigator>  
     </NavigationContainer>
-
     
   );
 
