7ac726f7b269f7c24ba2b746d50b3ebe6ae5d54c
Lkaasan <49850026+Lkaasan@users.noreply.github.com>
2022-12-02 12:25:57 +0000

    php files

--- backend/job.php
+++ backend/job.php
@@ -2,16 +2,14 @@
 include 'config.php';
 $json = file_get_contents('php://input');
 $obj = json_decode($json, true);
-
 $jobID = $obj['jobID'];
 
-if (isset($obj["userID"]) != ""){
+if (isset($obj["jobID"]) != ""){
     $query = "SELECT * FROM jobs WHERE jobID = " . $jobID;
     $result = $mysqli->query($query);
     if ($result->num_rows > 0){
         $row = mysqli_fetch_assoc($result);
-        echo $row["userID"];
-        echo $row["specialityID"];
+		echo json_encode(array($row['userID'], $row['specialityID'], $row['job_title'], $row['job_description'], $row['posted_date'], $row['job_accepted'], $row['job_completed'], $row['price']));
     } else {
         echo "job doesn't exist";
     }
--- backend/postThumbnail.php
+++ backend/postThumbnail.php
@@ -1,26 +1,41 @@
-<?php include config.php
+<?php 
+include 'config.php';
 	
 	$json = file_get_contents('php://input');
 	$obj = json_decode($json, true);
 	
 	$jobID = $obj['jobID'];
 	
-	if (isset($obj['jobID']) != ''){
+	//$jobDescription = $obj['job_description'];
+	
+	//if (isset($obj['jobID']) != ''){
 		$query = sprintf(
-		"SELECT * FROM jobs");
+			"SELECT * FROM jobs WHERE jobID= '%s'",
+			$mysqli->real_escape_string($jobID)
+			//$mysqli->real_escape_string($jobDescription)
+			);
 		
 		$result = $mysqli->query($query);
+		/*$rows = array();
+		//mysqli_fetch_assoc($result);
+		while($r = mysqli_fetch_assoc($result)) {
+			$rows[] = $r;
+			//echo json_encode[$r];
+		}*/
 		$row = mysqli_fetch_assoc($result);
+		$r = array($row['jobID'], $row['job_title'],$row['job_description'], $row['price']);
+		echo json_encode($r);
+		//echo json_encode($row['userID']);
 		if ($result->num_rows == 1) {
-			echo json_encode($result);
+		//echo json_encode($row[0]);
 		} else {
 			echo json_encode(-1);
 		}
-	} else {
-		echo json_encode('try again');
+		//} else {
+		//	echo json_encode('try again');
 		
 	
-	}
+	//}
 	
 	
 
