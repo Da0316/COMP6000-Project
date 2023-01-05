3e54179a310ec858fbf282101277384d8b9d0791
Lkaasan <49850026+Lkaasan@users.noreply.github.com>
2023-01-05 21:22:55 +0000

    select specialities page working

--- screens/selectSpecialities.js
+++ screens/selectSpecialities.js
@@ -1,25 +1,117 @@
-import React from 'react';
-import {} from 'react-native';
+import {useState, useEffect} from 'react';
+import {Text, View, StyleSheet, FlatList} from 'react-native';
+import Constants from 'expo-constants';
+//import CheckBox from '@react-native-community/checkbox';
+import {Card, Checkbox} from 'react-native-paper';
 
-function SelectSpecialities({route, navigation}){
-    const options = [];
-    const {userID} = route.params;
-    console.log(userID);
-    fetch('https://raptor.kent.ac.uk/proj/comp6000/project/08/Specialities.php', {
-        method: 'post',
-        header : {
-            Accept: 'application/json',
-            'Content-type' : 'application/json',
+export default SelectSpecialities = ({route, navigation}) => {
+  const [options, setOptions] = useState([]);
+  const {userID} = route.params;
+
+  useEffect(() => {
+    fetch('https://raptor.kent.ac.uk/proj/comp6000/project/08/specialities.php', {
+      method: 'post',
+      header: {
+        Accept: 'application/json',
+        'Content-type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        let counter = 0;
+        const newOptions = [];
+        for (let i = 0; i < responseJson.length / 2; i++) {
+          let object = {
+            id: Number(responseJson[counter]),
+            speciality: responseJson[counter + 1],
+            checked: false,
+          };
+          newOptions.push(object);
+          counter = counter + 2;
         }
-    })
-    .then((response) => response.json())
-    .then((responseJson) => {
-        console.log(responseJson);
-        console.log(userID);
-    })
-    .catch((error) => {
-        //alert(error)
-    });
-}
+        setOptions(newOptions);
+      })
+      .catch((error) => {
+        // alert(error);
+      });
+  }, []);
 
-export default SelectSpecialities;
+  const handleChange = (id) => {
+    const temp = options.map((option) => {
+      if (id === option.id) {
+        return {...option, checked: !option.checked};
+      }
+      return option;
+    });
+    setOptions(temp);
+  };
+
+  let selected = options.filter((option) => option.checked);
+
+  const renderList = (renderData) => {
+    return (
+      <FlatList
+        data={renderData}
+        renderItem={({item}) => (
+          <Card style={{margin: 5}}>
+            <View style={styles.card}>
+		        <View
+                style={{
+                  flexDirection: 'row',
+                  flex: 1,
+                  justifyContent: 'space-between',
+                }}>
+                <Checkbox
+                  status={item.checked ? 'checked' : 'unchecked'}
+                  onPress={() => {
+                    handleChange(item.id);
+                  }}
+                />
+                <Text>{item.speciality}</Text>
+              </View>
+            </View>
+          </Card>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={{flex: 1}}>{renderList(options)}</View>
+      <Text style={styles.text}>Selected</Text>
+      <View style={{flex: 1}}>{renderList(selected)}</View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+
+  card: {
+    padding: 10,
+    margin: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 5,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    elevation: 5,
+  },
+  text: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+});
